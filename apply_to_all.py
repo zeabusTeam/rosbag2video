55f6110eb61143d20df6c77c9f2fc32ff63e7f36
Phitchawat Lukkanathiti <me@shayennn.com>
2019-04-14 15:21:01 +0700

    Done multitask apply to many file

---
+++ apply_to_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2
+from file_util import scandir
+import sys
+import subprocess
+from multiprocessing import Pool
+
+
+def callMain(filename):
+    return subprocess.call(['./main.py', filename], shell=True)
+
+
+def main():
+    p = Pool(10)
+    path = '.'
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    baglist = scandir(path)
+    p.map(callMain, baglist)
+    print("Finished")
+
+
+if __name__ == "__main__":
+    main()
